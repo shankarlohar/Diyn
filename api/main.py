b54980283ac667e9e395f1e77023b0091477c233
shankarlohar <shankarloharmail@gmail.com>
2024-02-28 15:14:05 +0530

    feat(api/main.py): Create first API template

---
+++ api/main.py
@@ -0,0 +1,15 @@
+from fastapi import FastAPI
+from diynai import sql_chain
+from model import Query
+
+app = FastAPI()
+
+print("CHATBOT READY!")
+
+
+@app.get("/")
+async def root(token: Query):
+
+    result = sql_chain.run(token.question)
+
+    return result
